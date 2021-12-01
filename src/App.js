e4b216628b551ba2abc2efc6064503279c6e389e
Alex Wang <a3803896@gmail.com>
2021-12-01 11:34:29 +0800

    動畫長條圖 完工

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 // svg 設定
@@ -10,6 +10,7 @@ const innerWidth = width - margin.left - margin.right;
 
 export default function App() {
   // data
+  const svgRef = useRef();
   const [data, setData] = useState([]);
   // mounted
   useEffect(() => {
@@ -18,38 +19,44 @@ export default function App() {
   // watch
   useEffect(() => {
     if (!data.length) return;
-    const svg = d3.select('svg');
-    const g = svg.append('g').attr('id', 'maingroup').attr('transform', `translate(${margin.left},${margin.top})`);
     const xScale = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => +d['2020'])])
+      .domain([0, d3.max(data, (d) => +d['1950'])])
       .range([0, innerWidth]);
+    const xAxis = d3.axisBottom(xScale);
     const yScale = d3
       .scaleBand()
       .domain(data.map((d) => d.Country))
       .range([0, innerHeight])
       .padding(0.1);
-    const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
-    g.append('g').call(xAxis).attr('transform', `translate(0,${innerHeight})`);
-    g.append('g').call(yAxis);
+    const svg = d3.select(svgRef.current);
+    const g = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
+    g.append('g').attr('id', 'x-axis').attr('transform', `translate(0, ${innerHeight})`).call(xAxis);
+    g.append('g').attr('id', 'y-axis').call(yAxis);
     g.selectAll('rect')
       .data(data, (d) => d.Country)
       .enter()
       .append('rect')
-      .attr('width', (d) => xScale(+d['2020']))
+      .attr('width', (d) => xScale(+d['1950']))
       .attr('height', yScale.bandwidth())
-      .attr('fill', 'skyblue')
-      .attr('y', (d) => yScale(d.Country));
-    setTimeout(() => {
+      .attr('y', (d) => yScale(d.Country))
+      .attr('fill', 'skyblue');
+    function draw(year) {
+      xScale.domain([0, d3.max(data, (d) => +d[year])]);
+      d3.select('#x-axis').transition().duration(500).call(xAxis);
       g.selectAll('rect')
-        .data(data, (d) => d.Country)
         .transition()
-        .duration(800)
-        .attr('width', (d) => xScale(+d['1950']));
-    }, 1000);
+        .duration(500)
+        .attr('width', (d) => xScale(+d[year]));
+    }
+    let year = 1951;
+    let id = setInterval(() => {
+      if (year === 2020) clearInterval(id);
+      draw(year);
+      year++;
+    }, 20);
   }, [data]);
-  // methods
   async function getData() {
     const res = await d3.csv(
       'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
@@ -57,5 +64,5 @@ export default function App() {
     setData(res.slice(0, 10));
   }
   // template
-  return <svg width={width} height={height}></svg>;
+  return <svg ref={svgRef} width={width} height={height}></svg>;
 }
