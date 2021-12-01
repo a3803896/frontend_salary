73c875ed351f5e1c6c82b9bad48c8c24be6a1099
Alex Wang <a3803896@gmail.com>
2021-12-01 16:30:38 +0800

    移動的點 完工

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 // svg 設定
-const width = '1200';
-const height = '650';
-const margin = { top: 20, right: 20, bottom: 50, left: 140 };
+const width = '1100';
+const height = '600';
+const margin = { top: 20, right: 20, bottom: 70, left: 90 };
 const innerHeight = height - margin.top - margin.bottom;
 const innerWidth = width - margin.left - margin.right;
 
@@ -12,6 +12,12 @@ export default function App() {
   // data
   const svgRef = useRef();
   const [data, setData] = useState([]);
+  const xValue = (d) => d.cases;
+  const yValue = (d) => d.deaths;
+  const svg = d3.select(svgRef.current);
+  let xScale;
+  let yScale;
+  let sizeScale;
   // mounted
   useEffect(() => {
     getData();
@@ -19,49 +25,65 @@ export default function App() {
   // watch
   useEffect(() => {
     if (!data.length) return;
-    const xScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => +d['1950'])])
-      .range([0, innerWidth]);
-    const xAxis = d3.axisBottom(xScale);
-    const yScale = d3
-      .scaleBand()
-      .domain(data.map((d) => d.Country))
-      .range([0, innerHeight])
-      .padding(0.1);
-    const yAxis = d3.axisLeft(yScale);
-    const svg = d3.select(svgRef.current);
-    const g = svg.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
-    g.append('g').attr('id', 'x-axis').attr('transform', `translate(0, ${innerHeight})`).call(xAxis);
-    g.append('g').attr('id', 'y-axis').call(yAxis);
-    g.selectAll('rect')
-      .data(data, (d) => d.Country)
-      .enter()
-      .append('rect')
-      .attr('width', (d) => xScale(+d['1950']))
-      .attr('height', yScale.bandwidth())
-      .attr('y', (d) => yScale(d.Country))
-      .attr('fill', 'skyblue');
-    function draw(year) {
-      xScale.domain([0, d3.max(data, (d) => +d[year])]);
-      d3.select('#x-axis').transition().duration(500).call(xAxis);
-      g.selectAll('rect')
-        .transition()
-        .duration(500)
-        .attr('width', (d) => xScale(+d[year]));
-    }
-    let year = 1951;
-    let id = setInterval(() => {
-      if (year === 2020) clearInterval(id);
-      draw(year);
-      year++;
-    }, 20);
+    renderInit();
+    let c = 0;
+    let intervalId = setInterval(() => {
+      if (c === data.length) return clearInterval(intervalId);
+      draw(data[c]);
+      c++;
+    }, 50);
   }, [data]);
+  function draw(d) {
+    d3.select('circle')
+      .transition()
+      .duration(100)
+      .attr('cx', xScale(d.cases))
+      .attr('cy', yScale(d.deaths))
+      .attr('r', sizeScale(d.cases * d.deaths));
+  }
+  function renderInit() {
+    xScale = d3.scaleLinear().domain(d3.extent(data, xValue)).range([0, innerWidth]).nice();
+    yScale = d3.scaleLinear().domain(d3.extent(data, yValue).reverse()).range([0, innerHeight]).nice();
+    sizeScale = d3
+      .scaleLinear()
+      .domain(d3.extent(data, (d) => d.cases * d.deaths))
+      .range([10, 50]);
+    const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`).attr('id', 'maingroup');
+    const xAxis = d3.axisBottom(xScale).tickSize(-innerHeight).tickPadding(10);
+    const yAxis = d3.axisLeft(yScale).tickSize(-innerWidth).tickPadding(10);
+    const xAxisGroup = g.append('g').call(xAxis).attr('transform', `translate(0,${innerHeight})`);
+    const yAxisGroup = g.append('g').call(yAxis);
+    xAxisGroup
+      .append('text')
+      .text('確診人數')
+      .attr('font-size', '2em')
+      .attr('fill', 'black')
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(${innerWidth / 2},50)`);
+    yAxisGroup
+      .append('text')
+      .text('死亡人數')
+      .attr('font-size', '2em')
+      .attr('fill', 'black')
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(-40,${innerHeight / 2}) rotate(-90)`);
+    d3.select('#maingroup')
+      .append('circle')
+      .attr('cx', xScale(data[0].cases))
+      .attr('cy', yScale(data[0].deaths))
+      .attr('r', '10')
+      .attr('fill', 'black')
+      .attr('opacity', 0.8);
+  }
   async function getData() {
-    const res = await d3.csv(
-      'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
-    );
-    setData(res.slice(0, 10));
+    const format = (d) => {
+      d.date = new Date(d.date);
+      d.cases = +d.cases;
+      d.deaths = +d.deaths;
+      return d;
+    };
+    const res = await d3.csv('https://gist.githubusercontent.com/caracao718/1e8f6c76bc818a7c29946f53e3db745f/raw/data.csv', format);
+    setData(res);
   }
   // template
   return <svg ref={svgRef} width={width} height={height}></svg>;
